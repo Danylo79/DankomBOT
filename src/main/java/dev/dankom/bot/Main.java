3fc080e19c6b73d981bef2f6313aac22ae39e819
BuildTools <unconfigured@null.spigotmc.org>
2020-10-19 15:43:12 -0400

    Initial Commit

---
+++ src/main/java/dev/dankom/bot/Main.java
@@ -0,0 +1,15 @@
+package dev.dankom.bot;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
+import javax.security.auth.login.LoginException;
+
+public class Main {
+    private JDA jda;
+
+    public Main() throws LoginException {
+        String token = Token.token;
+        jda = JDABuilder.createDefault(token).build();
+    }
+}
---
+++ src/main/java/dev/dankom/bot/Token.java
@@ -0,0 +1,5 @@
+package dev.dankom.bot;
+
+public class Token {
+    public static String token = "NzY3ODMzMDU1MjU0OTM3NjAw.X43qQg.quGHRJJy9tTByGjIsOxqdb00Xkw";
+}
